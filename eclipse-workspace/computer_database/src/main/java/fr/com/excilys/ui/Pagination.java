63e82e11bc4d88137b3e1803b61d28a6e1a7276f
boukayou <mboukayou@excilys.com>
2019-02-22 17:41:10 +0100

    gros blocage avec la transition projet java to projet maven a resoudre

---
+++ eclipse-workspace/computer_database/src/main/java/fr/com/excilys/ui/Pagination.java
@@ -0,0 +1,28 @@
+package fr.com.excilys.ui;
+
+public class Pagination {
+	
+	public int maxElements;
+	public int offset ;
+	
+	public Pagination(int maxElements, int offset) {
+		super();
+		this.maxElements = maxElements;
+		this.offset = offset;
+	}
+		
+	public void previous() {
+				
+	}
+			
+	public void next() {
+				
+	}
+	
+	public void maxTimes() {
+		
+	}
+		
+	
+
+}
